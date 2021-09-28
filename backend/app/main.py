4bdb38ddcd90800c6f10d3d0eb457f2fee3cb91b
sarvadnya <scikido@github.com>
2021-09-28 05:57:43 +0530

    Procfile

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,11 +4,10 @@ load_dotenv()
 from flask import Flask , render_template
 
 app=Flask(__name__)
-def login_required(function):
-    pass
+
+
 
 @app.route('/')
-@login_required
 def hello():
     return render_template('index.html')
  
