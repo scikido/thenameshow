9e189490ad792e01961835257a00fa5e81b0ca62
sarvadnya <scikido@github.com>
2021-09-30 15:27:30 +0530

    final folder structure

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
-from flask import Flask , render_template
+from flask_login import LoginManager , login_user, logout_user , login_required
 
+from flask import Flask , render_template , session
+load_dotenv()
 app=Flask(__name__)
 
 
-
-@app.route('/',methods=['POST','GET'])
+@app.route('/')
 def hello():
     return render_template('index.html')
 
+
+@app.route('/blog')
+def login():
+    return render_template('blog.html')
+
+
+def logout():
+    pass
+
+def callback():
+    pass
